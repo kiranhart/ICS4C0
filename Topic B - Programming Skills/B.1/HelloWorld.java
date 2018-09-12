de1d4426c1a48e2df32844bd86d1f2ca6b7edfa0
Kiran Hart <kiran.hart@Live.com>
2018-09-12 14:29:29 -0400

    Create HelloWorld.java

---
+++ Topic B - Programming Skills/B.1/HelloWorld.java
@@ -0,0 +1,18 @@
+//The class HelloWorld is located in the following folder listing - com.kiranhart.ics4c0.b1
+package com.kiranhart.ics4c0.b1;
+
+/*
+ * This is the outer structure of the class called HelloWorld
+ */
+public class HelloWorld {
+
+	/*
+	 * This is the main method of a java program, every single java program must have this, when
+	 * you click run, it looks for this method, the runs everything inside of it.
+	 */
+	public static void main(String[] args) {
+		
+		//This is just printing a new line to the console, with the text "Hello World"
+		System.out.println("Hello World!");
+	}
+}
