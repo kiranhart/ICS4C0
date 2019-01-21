65fe34b5bd941563f7b53f17599cde37177ff496
Kiran Hart <kiran.hart@Live.com>
2019-01-21 14:49:15 -0500

    Create Factorial.java

---
+++ Summative Task/Day 1 Task - OOP/Factorial.java
@@ -0,0 +1,31 @@
+
+public class Factorial {
+
+	//Store the number passed by constructor for later use
+	private int number;
+	
+	//The only constructor will take in a double value, and will f
+	public Factorial(int number) {
+		//Assign the number variable to the number passed through the constructor
+		this.number = number;
+	}
+	
+	//Calculate the factorial of the number using a simple algorithm
+	public int getResult() {
+		int val = 1;
+		for (int i = 1; i <= number; i++) {
+			val *= i;
+		}
+		return val;
+	}
+	
+	//Get the number
+	public int getNumber() {
+		return this.number;
+	}
+	
+	//Set the number
+	public void setNumber(int number) {
+		this.number = number;
+	}
+}
